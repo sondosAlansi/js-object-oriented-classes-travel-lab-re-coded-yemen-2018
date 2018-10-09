5a963a328cc3ae3349c52c70466c0690a5fc0f26
sondos alansi <sondos.alansi@yahoo.com>
2018-10-09 19:04:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,11 +32,11 @@ class Route{
     let numerMinuts;
     
     if(peakHour===true){
-      numerMinuts=this.blocksTravelled()/3;
+      numerMinuts=this.blocksTravelled()/2;
       return numerMinuts;
     }
     else{
-       numerMinuts=this.blocksTravelled()/2;
+       numerMinuts=this.blocksTravelled()/3;
       return numerMinuts;
     }
   }
