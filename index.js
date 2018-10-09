00868454c86c2b9f8a3a96fa35785a3eac6fe23b
sondos alansi <sondos.alansi@yahoo.com>
2018-10-09 18:17:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-
+class Driver{
+  
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date('95');
+  }
+ 
+  
+ 
+  yearsExperienceFromBeginningOf(year){
+    this.startDate=this.startDate.getFullYear();
+    return year-this.startDate ;
+  }
+}
+class Route{
+   constructor(beginningLocation , endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+  blocksTravelled(){
+  //  return Number.parseInt(this.endingLocation.vertical)- //Number.parseInt(this.beginningLocation.vertical);
+  }
+  
+}
